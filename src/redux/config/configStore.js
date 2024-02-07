a9838a847d56f2af010e756dacaa1c899fcba57e
heedong12 <dlgmldus3289@naver.com>
2024-02-08 02:35:58 +0900

    [chore]redux-logger 설정

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,8 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { combineReducers } from "redux";
 import ex from "../modules/ex.js";
+import { createLogger } from "redux-logger";
 
 const rootReducer = combineReducers({ ex });
-const store = createStore(rootReducer);
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;

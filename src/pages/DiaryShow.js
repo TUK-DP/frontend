5eb0ba3213e56b12fe5e922598b917acdf0d74cd
heedong12 <dlgmldus3289@naver.com>
2024-02-11 17:54:43 +0900

    Merge branch 'feat/4' of https://github.com/TUK-DP/frontend into feat/4

--- src/pages/DiaryShow.js
+++ src/pages/DiaryShow.js
@@ -1,19 +1,1 @@
-import { useLocation, useNavigate } from "react-router-dom";
-
-const DiaryShow = () => {
-  const location = useLocation();
-  const image = location.state?.imageDataUrl || null;
-
   return (
-    <div>
-      {image && (
-        <img
-          src={image}
-          style={{ width: "100px", height: "100px", marginTop: "80px" }}
-          alt="Captured"
-        />
-      )}
-    </div>
-  );
-};
-export default DiaryShow;

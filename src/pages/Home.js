36a4d1e4508f2b3d554b3015fd1b94423638259a
Hj1218 <wjdguswl2341@naver.com>
2024-02-22 16:19:04 +0900

    [feat] 홈 메인 버튼 캘린더 페이지 이동 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -35,7 +35,7 @@ const Home = () => {
           </div>
           <div className="btn_text">게임</div>
         </div>
-        <div className="mainBtn">
+        <div className="mainBtn" onClick={() => {navigate("/calendar");}}>
           <div className="btn_icon">
             <img src={mainBtn3}></img>
           </div>

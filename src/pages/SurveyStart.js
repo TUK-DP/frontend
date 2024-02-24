4eb2492d1db62e24fcfc1a4743ba809bc98490ec
Hj1218 <wjdguswl2341@naver.com>
2024-02-24 21:43:06 +0900

    [design] 배경 색, 테두리, 폰트사이즈 수정

--- src/pages/SurveyStart.js
+++ src/pages/SurveyStart.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import Check from '../assets/check.png';
+import '../styles/Survey.css';
 
 const SurveyStart = () => {
   return (
     <div>
-      <div>
+      <div id='nameBox'>
         <img src={Check} height="95"/>
-        <p>치매 자가 진단법</p>
-        <p>총 32문항</p>
+        <p style={{fontSize:"35px", fontWeight:"bold"}}>치매 자가 진단법</p>
+        <p style={{fontSize:"25px"}}>총 32문항</p>
       </div>
-      <p>발병 전후의 상태를 잘 아는 본인이 치매환자의 인지기능에 대한 정보를 제공함으로써 치매를 초기에 발견할 수 있는 치매 선별검사</p>
-      <div>
-        <p>본 검사는 자가진단용입니다.</p>
-        <p>정확한 진단이 필요하다면 전문의의 도움을 받으세요.</p>
-        <div>치매 진단하기</div>
-        <div>이전 진단결과</div>
+      <p id='txtBox'>발병 전후의 상태를 잘 아는 본인이 <br/> 치매환자의 인지기능에 대한 정보를 <br/> 제공함으로써 치매를 조기에 <br/> 발견할 수 있는 치매 선별검사</p>
+      <div id='btnSurveyBox'>
+        <p style={{fontSize:"15px"}}>본 검사는 자가진단용입니다. <br/> 정확한 진단이 필요하다면 전문의의 도움을 받으세요.</p>
+        <div id='btn_survey'>치매 진단하기</div>
+        <div id='btn_before'>이전 진단결과</div>
       </div>
     </div>
   );

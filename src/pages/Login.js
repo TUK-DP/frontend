8573c07fc8909eb02e3f98af2c8a7c77f1ac5115
heedong12 <dlgmldus3289@naver.com>
2024-02-29 19:07:54 +0900

    [feat]로그인, 회원가입 css 수정

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,23 +2,45 @@ import "../styles/Login.css";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const onSubmit = (data) => {
+    if (
+      !data.username ||
+      !data.nickname ||
+      !data.birth ||
+      !data.email ||
+      !data.password ||
+      !data.passwordChk
+    ) {
+      alert("빈 칸 없이 작성해주세요.");
+      return;
+    }
+
+    // 나머지 제출 로직
+    console.log(data);
+  };
   return (
     <div id="screen">
-      <form>
-        <div className="inputField">
+      <form onSubmit={onSubmit}>
+        <div className="inputField my-5">
           <label>이름</label>
-          <input type="text" placeholder="이름을 입력하세요." />
+          <input type="text" placeholder="이름을 입력하세요." id="username" />
         </div>
-        <div className="inputField">
+        <div className="inputField my-5">
           <label>비밀번호</label>
-          <input type="password" placeholder="비밀번호를 입력하세요." />
+          <input
+            type="password"
+            placeholder="비밀번호를 입력하세요."
+            id="password"
+          />
         </div>
+        <button id="loginBtn" type="submit" className={"mt-10"}>
+          로그인
+        </button>
       </form>
-      <button id="loginBtn">로그인</button>
       <div id="container">
         <div>회원이 아니신가요?</div>
         <Link to="/signup" style={{ color: "inherit" }}>
-          <div style={{}}>회원가입</div>
+          <div>회원가입</div>
         </Link>
       </div>
     </div>
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,37 +1,67 @@
 import "../styles/Login.css";
-
+import { useForm } from "react-hook-form";
 const Signup = () => {
+  const onSubmit = (data) => {
+    if (
+      !data.username ||
+      !data.nickname ||
+      !data.birth ||
+      !data.email ||
+      !data.password ||
+      !data.passwordChk
+    ) {
+      alert("빈 칸 없이 작성해주세요.");
+      return;
+    }
+
+    // 나머지 제출 로직
+    console.log(data);
+  };
   return (
     <div id="screen">
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="inputField">
           <label>이름</label>
-          <input type="text" placeholder="이름을 입력하세요." />
+          <input type="text" placeholder="이름을 입력하세요." id="username" />
+          <div className="error-message"></div>
         </div>
         <div className="inputField">
           <label>닉네임</label>
-          <input type="text" placeholder="닉네임을 입력하세요." />
+          <input type="text" placeholder="닉네임을 입력하세요." id="nickname" />
+          <div className="error-message"></div>
         </div>
         <div className="inputField">
           <label>생년월일</label>
-          <input type="date" placeholder="생년월일을 입력하세요." />
+          <input type="date" placeholder="생년월일을 입력하세요." id="birth" />
+          <div className="error-message"></div>
         </div>
         <div className="inputField">
           <label>이메일</label>
-          <input type="email" placeholder="이메일을 입력하세요." />
+          <input type="email" placeholder="이메일을 입력하세요." id="email" />
+          <div className="error-message"></div>
         </div>
         <div className="inputField">
           <label>비밀번호</label>
-          <input type="password" placeholder="비밀번호를 입력하세요." />
+          <input
+            type="password"
+            placeholder="비밀번호를 입력하세요."
+            id="password"
+          />
+          <div className="error-message"></div>
         </div>
         <div className="inputField">
           <label>비밀번호 확인</label>
-          <input type="password" placeholder="비밀번호를 입력하세요." />
+          <input
+            type="password"
+            placeholder="비밀번호를 입력하세요."
+            id="passwordChk"
+          />
+          <div className="error-message"></div>
         </div>
+        <button id="loginBtn" type="submit">
+          완료
+        </button>
       </form>
-      <button id="loginBtn" style={{ marginTop: "30px" }}>
-        완료
-      </button>
     </div>
   );
 };

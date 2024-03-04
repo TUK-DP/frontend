a5325b368a7a249dc566b5f90f8187423ce89aea
heeyeon <dlgmldus3289@naver.com>
2024-03-04 14:24:25 +0900

    [design]일기장 작성 부분 디자인 수정

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,27 +1,54 @@
-import React from "react";
-import '../styles/diary.css'
+import React, { useCallback, useRef } from "react";
+import "../styles/diary.css";
 import { useNavigate } from "react-router-dom";
 
 const Diary = () => {
   const navigate = useNavigate();
+  const textRef = useRef();
+  const handleResizeHeight = useCallback(() => {
+    textRef.current.style.height = textRef.current.scrollHeight + "px";
+  }, []);
 
-  function autoResize(event){
-    const writeBox = event.target;
-    writeBox.style.height = 'auto';
-    writeBox.style.height = writeBox.scrollHeight + 'px';
-  }
-
-  return(
+  return (
     <div id="diary">
       <div id="date">2월 22일</div>
       <div id="draw">
-        <div id="btn_paint" onClick={() => {navigate("/draw");}}>그림 그리기</div>
+        <div
+          id="btn_paint"
+          onClick={() => {
+            navigate("/draw");
+          }}
+        >
+          그림 그리기
+        </div>
       </div>
       <div id="contentBox">
-        <div style={{fontSize:"23px", fontWeight:"bold", margin:"20px 0", textAlign:"left", width:"85%"}}>오늘의 일기</div>
-        <textarea id="writeBox" placeholder="일기를 작성해주세요." rows={30} onKeyUp={autoResize} onKeyDown={autoResize}></textarea>
+        <div
+          style={{
+            fontSize: "23px",
+            fontWeight: "bold",
+            margin: "20px 0",
+            textAlign: "left",
+            width: "85%",
+          }}
+        >
+          오늘의 일기
+        </div>
+        <textarea
+          id="writeBox"
+          placeholder="일기를 작성해주세요."
+          ref={textRef}
+          onInput={handleResizeHeight}
+        ></textarea>
       </div>
-      <div id="btn_save" onClick={() => {navigate("/calendar");}}>저장</div>
+      <div
+        id="btn_save"
+        onClick={() => {
+          navigate("/calendar");
+        }}
+      >
+        저장
+      </div>
     </div>
   );
 };

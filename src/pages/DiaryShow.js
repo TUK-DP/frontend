565916a1d032d2028ce81b40abdad8eda27118f7
heeyeon <dlgmldus3289@naver.com>
2024-03-07 02:15:04 +0900

    [feat]일기 열기/닫기 기능 구현

--- src/pages/DiaryShow.js
+++ src/pages/DiaryShow.js
@@ -1,49 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/DiaryEdit.css";
-import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import Diary from "../pages/Diary.js";
 
 const DiaryEdit = () => {
-  const navigate = useNavigate();
-  const { year, month, day } = useSelector((state) => state.DiaryDate);
-
+  //true -> 일기열기, false -> 일기닫기
+  const [showDiary, setShowDiary] = useState(false);
+  const toggleBtn = () => {
+    setShowDiary(!showDiary);
+  };
   return (
-    <div
-      className={
-        "flex justify-between items-center py-8 bg-[#e0f4ff] rounded-2xl "
-      }
-    >
-      <button
-        className={
-          "bg-[#82aae3] text-white w-32 h-10 flex justify-center items-center rounded-full font-bold text-lg mx-2"
-        }
-        style={{ boxShadow: " 3px 3px 3px rgb(200, 200, 200)" }}
-      >
-        일기회상
-      </button>
-      <button
-        className={
-          "bg-[#82aae3] text-white w-32 h-10 flex justify-center items-center rounded-full font-bold text-lg mx-2"
-        }
-        style={{ boxShadow: " 3px 3px 3px rgb(200, 200, 200)" }}
-      >
-        일기보기
-      </button>
+    <div>
+      <div className={"flex justify-between items-center py-8 bg-[#e0f4ff]  "}>
+        <button
+          className={
+            "bg-[#82aae3] text-white w-32 h-10 flex justify-center items-center rounded-full font-bold text-lg mx-2"
+          }
+          style={{ boxShadow: " 3px 3px 3px rgb(200, 200, 200)" }}
+        >
+          일기회상
+        </button>
+        <button
+          className={
+            "bg-[#82aae3] text-white w-32 h-10 flex justify-center items-center rounded-full font-bold text-lg mx-2"
+          }
+          style={{ boxShadow: " 3px 3px 3px rgb(200, 200, 200)" }}
+          onClick={toggleBtn}
+        >
+          {showDiary ? "일기닫기" : "일기열기"}
+        </button>
+      </div>
+      {showDiary && <Diary />}
     </div>
-    // <div id="edit">
-    //   <div id="btnBox">
-    //     <div className="btn" onClick={() => navigate("/diary/test")}>
-    //       일기회상
-    //     </div>
-    //     <div className="btn">일기보기</div>
-    //   </div>
-    //   <div id="textBox">{`${month}월 ${day}일`}</div>
-    //   <div id="painting"></div>
-    //   <div id="diaryContent">
-    //     <div id="title">제목</div>
-    //     <div id="content">일기내용</div>
-    //   </div>
-    // </div>
   );
 };
 

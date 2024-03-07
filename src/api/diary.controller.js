4f4f36d0eeca447b1e61b2a9ac656226fbd3cd5b
heeyeon <dlgmldus3289@naver.com>
2024-03-07 12:28:42 +0900

    [feat]Diary API Controller 작성

--- src/api/diary.controller.js
+++ src/api/diary.controller.js
@@ -1,9 +1,21 @@
-import {Api} from "./common.controller";
+import { Api } from "./common.controller";
 
 class DiaryController extends Api {
-    findById = async ({diaryId=0}) => {
-        return await this.get(`/diary/${diaryId}`);
-    }
+  //유저의 일기 조회
+  findById = async ({ userId = 0 }) => {
+    return await this.get(`/diary/list/userId=?${userId}`);
+  };
+  //일기 작성
+  writeDiary = async (diaryData) => {
+    return await this.post("/diary/write", { data: diaryData });
+  };
+  //일기 수정
+  updateDiary = async (diaryData) => {
+    return await this.post("/diary/update", { data: diaryData });
+  };
+  //일기회상 퀴즈
+  getQuiz = async ({ diaryId = 0 }) => {
+    return await this.get(`/diary/quiz?diaryId=${diaryId}`);
+  };
 }
-
 export default new DiaryController();

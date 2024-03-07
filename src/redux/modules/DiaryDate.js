7b444d9794b3bc9086202773a28722ed540387ff
heeyeon <dlgmldus3289@naver.com>
2024-03-07 16:51:49 +0900

    Revert "[feat]리덕스 수정(월,일 앞에 0 붙임)"
    This reverts commit 03f89fcdeb4d915d410d6e98b818d08a52733475.

--- src/redux/modules/DiaryDate.js
+++ src/redux/modules/DiaryDate.js
@@ -3,9 +3,9 @@ export const CHANGE_MONTH = "CHANGE_MONTH";
 export const CHANGE_DAY = "CHANGE_DAY";
 
 const initialState = {
-  year: new Date().getFullYear().toString(),
-  month: (new Date().getMonth() + 1).toString().padStart(2, "0"),
-  day: new Date().getDate().toString().padStart(2, "0"),
+  year: parseInt(new Date().getFullYear(), 10),
+  month: parseInt(new Date().getMonth() + 1, 10),
+  day: parseInt(new Date().getDate(), 10),
 };
 
 export default function DiaryDate(state = initialState, action) {
@@ -13,37 +13,37 @@ export default function DiaryDate(state = initialState, action) {
     // case CHANGE_YEAR:
     //   return {
     //     ...state,
-    //     year: (parseInt(state.year, 10) + action.number).toString(),
+    //     year: state.year + action.number,
     //   };
     case CHANGE_MONTH:
-      const newMonth = parseInt(state.month, 10) + action.number;
+      const newMonth = state.month + action.number;
       // 월이 1 미만이면 전 년도 12월로 설정
       if (newMonth < 1) {
         return {
           ...state,
-          year: (parseInt(state.year, 10) - 1).toString(),
-          month: "12",
+          year: state.year - 1,
+          month: 12,
         };
       }
       // 월이 12 초과면 다음 년도 1월로 설정
       else if (newMonth > 12) {
         return {
           ...state,
-          year: (parseInt(state.year, 10) + 1).toString(),
-          month: "01",
+          year: state.year + 1,
+          month: 1,
         };
       }
       // 그 외의 경우는 그대로 업데이트
       else {
         return {
           ...state,
-          month: newMonth.toString().padStart(2, "0"),
+          month: newMonth,
         };
       }
     case CHANGE_DAY:
       return {
         ...state,
-        day: action.number.toString().padStart(2, "0"),
+        day: action.number,
       };
 
     default:

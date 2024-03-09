4799f1e89f03fbda81d0b00cfb00b7c8ba071666
heedong12 <dlgmldus3289@naver.com>
2024-03-09 18:57:11 +0900

    [refactor]일기 작성 API 수정

--- src/pages/DiaryWrite.js
+++ src/pages/DiaryWrite.js
@@ -27,11 +27,11 @@ const Diary = () => {
     return (
       date.getFullYear() +
       "-" +
-      (date.getMonth() + 1 < 9
+      (date.getMonth() + 1 <= 9
         ? "0" + (date.getMonth() + 1)
         : date.getMonth() + 1) +
       "-" +
-      (date.getDate() < 9 ? "0" + date.getDate() : date.getDate())
+      (date.getDate() <= 9 ? "0" + date.getDate() : date.getDate())
     );
   };
 

ef4da9de100a8ca36e7b13c80c348d740a17489e
heedong12 <dlgmldus3289@naver.com>
2024-03-09 19:19:18 +0900

    [refactor]일기 수정 post에서 patch로 수정

--- src/api/common.controller.js
+++ src/api/common.controller.js
@@ -1,33 +1,36 @@
 import axios from "axios";
 
 export class Api {
-    base_url = process.env.REACT_APP_SERVER_URL;
-    axiosInstance;
+  base_url = process.env.REACT_APP_SERVER_URL;
+  axiosInstance;
 
+  constructor() {
+    this.axiosInstance = axios.create({
+      baseURL: this.base_url,
+    });
+  }
 
-    constructor() {
-        this.axiosInstance = axios.create({
-            baseURL: this.base_url,
-        });
-    }
+  async sendRequest(options) {
+    const { method, url, data, content_type } = options;
 
-    async sendRequest(options) {
-        const {method, url, data, content_type} = options;
+    const config = {
+      headers: {
+        "Content-Type": content_type,
+      },
+    };
 
-        const config = {
-            headers: {
-                'Content-Type': content_type,
-            },
-        };
+    return await this.axiosInstance[method](url, data, config);
+  }
 
-        return await this.axiosInstance[method](url, data, config);
-    }
+  async get(url, { data, content_type = "application/json" } = {}) {
+    return await this.sendRequest({ method: "get", url, data, content_type });
+  }
 
-    async get(url, {data, content_type = 'application/json'} = {}) {
-        return await this.sendRequest({method: 'get', url, data, content_type});
-    }
+  async post(url, { data, content_type = "application/json" } = {}) {
+    return await this.sendRequest({ method: "post", url, data, content_type });
+  }
 
-    async post(url, {data, content_type = 'application/json'} = {}) {
-        return await this.sendRequest({method: 'post', url, data, content_type});
-    }
+  async patch(url, { data, content_type = "application/json" } = {}) {
+    return await this.sendRequest({ method: "patch", url, data, content_type });
+  }
 }

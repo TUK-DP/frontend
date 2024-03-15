f397faf0296297d57ce37a1af607b2bf48678cad
heedong12 <dlgmldus3289@naver.com>
2024-03-15 16:39:47 +0900

    [feat]Loading fixed로 고정

--- src/component/Loading.js
+++ src/component/Loading.js
@@ -2,9 +2,11 @@ import React from "react";
 import Spinner from "../assets/Rolling-1.4s-254px.gif";
 const Loading = ({ text }) => {
   return (
-    <div className="absolute bg-black bg-opacity-50 flex flex-col justify-center items-center w-4/5 h-1/3 top-[30%] left-[10%]">
-      <img src={Spinner} width="60%" alt="LOADING" />
-      <div className={"text-white font-extrabold text-3xl"}>{text}</div>
+    <div className="fixed inset-0 flex justify-center items-center z-50">
+      <div className="absolute bg-black bg-opacity-50 flex flex-col justify-center items-center w-4/5 h-1/3 top-[30%] left-[10%]">
+        <img src={Spinner} width="60%" alt="LOADING" />
+        <div className={"text-white font-extrabold text-3xl"}>{text}</div>
+      </div>
     </div>
   );
 };

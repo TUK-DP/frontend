aa05942419b9fdada674e38af70bb8b05f687485
Hj1218 <wjdguswl2341@naver.com>
2024-03-27 17:36:25 +0900

    [refactor] 스크롤 수정 필요

--- src/component/ScrollToTop.js
+++ src/component/ScrollToTop.js
@@ -1,13 +1,15 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-export default function ScrollToTop({ children }) {
+const ScrollToTop = (props) => {
   const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
+    console.log("scroll to top!");
   }, [pathname]);
 
-  // children을 렌더링하여 라우터로 연결된 페이지를 보여줍니다.
-  return children;
+  return <>{props.children}</>;
 }
+
+export default ScrollToTop;

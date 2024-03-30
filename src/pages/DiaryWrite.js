bc5f6ec68a5e2d66e46969650a2dd695ba4c086d
heedong12 <dlgmldus3289@naver.com>
2024-03-30 21:32:58 +0900

    [refactor]일기 작성 디자인 변경

--- src/pages/DiaryWrite.js
+++ src/pages/DiaryWrite.js
@@ -1,28 +1,19 @@
-import React, { useCallback, useRef, useState } from "react";
-import "../styles/diary.css";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import DiaryController from "../api/diary.controller";
 import { useSelector } from "react-redux";
 import Loading from "../component/Loading";
-//Diary 새롭게 작성
+import Button from "../component/Button";
+//일기 작성 페이지
 const Diary = () => {
   const navigate = useNavigate();
-  const textRef = useRef();
   const { year, month, day } = useSelector((state) => state.DiaryDate);
   const [isSaving, setIsSaving] = useState(false);
-
-  const handleResizeHeight = useCallback(() => {
-    const textarea = textRef.current;
-    textarea.style.height = "auto"; // Reset height to auto
-    textarea.style.height = textarea.scrollHeight + "px";
-  }, []);
-
-  const handleContentChange = (e) => {
-    // 내용이 변경될 때마다 높이 조정
-    handleResizeHeight();
-    setDiary({ ...diary, content: e.target.value });
+  //textarea의 내용을 받아오는 함수
+  const handleContentChange = (event) => {
+    setDiary({ ...diary, content: event.target.value });
   };
-
+  //api 요청을 위해 날짜를 포맷팅
   const dateFormat = () => {
     const date = new Date(year, month - 1, day);
     return (
@@ -35,15 +26,16 @@ const Diary = () => {
       (date.getDate() <= 9 ? "0" + date.getDate() : date.getDate())
     );
   };
-
+  //api 요청 시, body 형식
   const [diary, setDiary] = useState({
     userId: 2,
     title: "string",
     content: "",
     date: dateFormat(),
   });
-
+  //일기 저장 api 호출
   const saveDiary = async () => {
+    console.log("눌림");
     if (diary.content === "") {
       return alert("내용을 작성해주세요.");
     }
@@ -54,31 +46,41 @@ const Diary = () => {
   };
 
   return (
-    <div id="diary">
+    <div
+      className={
+        "flex flex-col bg-[#e0f4ff] h-full w-full justify-center items-center"
+      }
+    >
       {isSaving ? <Loading text="일기 저장 중..." /> : null}
-      <div id="contentBox" className={"mt-10"}>
-        <div
-          style={{
-            fontSize: "23px",
-            fontWeight: "bold",
-            margin: "20px 0",
-            textAlign: "left",
-            width: "85%",
-          }}
-        >
-          {diary.date}
-        </div>
-        <textarea
-          id="writeBox"
-          placeholder="일기를 작성해주세요."
-          ref={textRef}
-          onChange={handleContentChange}
-          value={diary.content}
-        ></textarea>
+      <div
+        style={{
+          fontSize: "23px",
+          fontWeight: "bold",
+          margin: "20px 0",
+          textAlign: "left",
+          width: "85%",
+        }}
+      >
+        {diary.date}
       </div>
-      <button id="btn_save" onClick={saveDiary} disabled={isSaving}>
-        작성완료
-      </button>
+      <textarea
+        className={
+          "w-11/12 h-full rounded-3xl text-xl p-4 font-bold focus:outline-none"
+        }
+        placeholder="일기를 작성해주세요."
+        value={diary.content}
+        onChange={handleContentChange}
+      ></textarea>
+      <div className={"flex flex-row w-11/12 gap-2 my-5"}>
+        <Button width="50%" height="50px" text="음성작성" />
+        <Button
+          width="50%"
+          height="50px"
+          text="작성완료"
+          onClick={saveDiary}
+          disabled={isSaving}
+        />
+      </div>
     </div>
   );
 };

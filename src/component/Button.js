10bf458443cc6d725702f58337fd449947f7e403
heedong12 <dlgmldus3289@naver.com>
2024-03-30 21:26:08 +0900

    [refactor]onClick 안되는 오류 해결

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const Button = ({ width, height, text }) => {
+const Button = ({ width, height, text, onClick }) => {
   return (
     <div
       className={
-        "flex justify-center items-center bg-[#82aae3] h-10 text-white rounded-xl text-xl font-bold "
+        "flex justify-center items-center bg-[#82aae3] h-10 text-white rounded-xl text-xl font-bold"
       }
       style={{ width: `${width}`, height: `${height}` }}
+      onClick={onClick}
     >
       {text}
     </div>

bad3e620fb8d0963feee8170f070fe77c36a3578
heedong12 <dlgmldus3289@naver.com>
2024-04-01 17:15:08 +0900

    [feat]게임 시작 전 설명 띄워주기

--- src/component/BeforeGame.js
+++ src/component/BeforeGame.js
@@ -1,15 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../component/Button";
 import { useLocation, useNavigate } from "react-router-dom";
+import Modal from "./Modal";
 
 const BeforeGame = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const num = location.state.num;
+  const gameInfo = location.state.gameInfo;
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   const goStartGame = () => {
     navigate(`/game${num}`);
   };
+
+  useEffect(() => {
+    console.log(num, gameInfo);
+  });
   return (
     <div
       className={
@@ -22,8 +33,16 @@ const BeforeGame = () => {
         text="게임 시작하기"
         onClick={goStartGame}
       />
-      <Button width="100%" height="60px" text="게임 설명보기" />
+      <Button
+        width="100%"
+        height="60px"
+        text="게임 설명보기"
+        onClick={() => {
+          setIsModalOpen(!isModalOpen);
+        }}
+      />
       <Button width="100%" height="60px" text="랭킹 보기" />
+      {isModalOpen && <Modal onClose={closeModal} content={gameInfo} />}
     </div>
   );
 };
--- src/pages/Games.js
+++ src/pages/Games.js
@@ -5,14 +5,22 @@ import gameImg3 from "../assets/gameImg3.png";
 import gameImg4 from "../assets/gameImg4.png";
 import gameImg5 from "../assets/gameImg5.png";
 import { useNavigate } from "react-router-dom";
-import BeforeGame from "../component/BeforeGame";
 
 const Games = () => {
   const navigate = useNavigate();
 
   const goBeforeGame = (num) => {
-    navigate("/beforegame", { state: { num } });
+    console.log(gameInfo[1]);
+    navigate("/beforegame", { state: { num, gameInfo: gameInfo[num - 1] } });
   };
+
+  const gameInfo = [
+    "이모티콘 찾기는 화면에 뜨는 이모티콘과 똑같은 모양이 이모티콘들을 선택하는 게임입니다!",
+    "지는 가위바위보는 지는 사람이 승자입니다! 만약 컴퓨터가 보를 냈을 때 가위가 아닌 주먹을 내야 이길 수 있습니다!",
+    "컬러매치는 단어의 색에 알맞는 보기를 선택하는 것입니다! 단어가 검정이라고 해도 단어의 색이 파랑이라면 파랑을 선택해야합니다!",
+    "순서대로 터치는 화면에 뜨는 다양한 내용들을 순서대로 터치하는 게임입니다! 내용은 숫자일수도 알파벳일수도 가나다라일수도 있습니다!",
+    "알맞은 글자연결은 두개의 카드를 골라서 알맞은 하나의 단어를 완성시키는 게임입니다!",
+  ];
 
   return (
     <div id="game">

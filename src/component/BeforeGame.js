ee90dcd449e5adf2633873a512737229a4bc500c
heedong12 <dlgmldus3289@naver.com>
2024-04-01 16:03:11 +0900

    [feat]게임 별 게임시작하기 연결

--- src/component/BeforeGame.js
+++ src/component/BeforeGame.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Button from "../component/Button";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const BeforeGame = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const num = location.state.num;
+
+  const goStartGame = () => {
+    navigate(`/game${num}`);
+  };
   return (
     <div
       className={
         "flex flex-col justify-center items-center gap-5 my-auto h-full mx-5"
       }
     >
-      <Button width="100%" height="60px" text="게임 시작하기" />
+      <Button
+        width="100%"
+        height="60px"
+        text="게임 시작하기"
+        onClick={goStartGame}
+      />
       <Button width="100%" height="60px" text="게임 설명보기" />
       <Button width="100%" height="60px" text="랭킹 보기" />
     </div>
--- src/pages/Games.js
+++ src/pages/Games.js
@@ -5,29 +5,59 @@ import gameImg3 from "../assets/gameImg3.png";
 import gameImg4 from "../assets/gameImg4.png";
 import gameImg5 from "../assets/gameImg5.png";
 import { useNavigate } from "react-router-dom";
+import BeforeGame from "../component/BeforeGame";
 
 const Games = () => {
   const navigate = useNavigate();
 
+  const goBeforeGame = (num) => {
+    navigate("/beforegame", { state: { num } });
+  };
+
   return (
     <div id="game">
-      <div className="btn_games" onClick={() => navigate("/game1")}>
+      <div
+        className="btn_games"
+        onClick={() => {
+          goBeforeGame(1);
+        }}
+      >
         <img src={gameImg1} />
         <div className="game_text">이모티콘 찾기</div>
       </div>
-      <div className="btn_games" onClick={() => navigate("/game2")}>
+      <div
+        className="btn_games"
+        onClick={() => {
+          goBeforeGame(2);
+        }}
+      >
         <img src={gameImg2} />
         <div className="game_text">지는 가위바위보</div>
       </div>
-      <div className="btn_games" onClick={() => navigate("/game3")}>
+      <div
+        className="btn_games"
+        onClick={() => {
+          goBeforeGame(3);
+        }}
+      >
         <img src={gameImg3} />
         <div className="game_text">컬러매치</div>
       </div>
-      <div className="btn_games" onClick={() => navigate("/game4")}>
+      <div
+        className="btn_games"
+        onClick={() => {
+          goBeforeGame(4);
+        }}
+      >
         <img src={gameImg4} />
         <div className="game_text">순서대로 터치</div>
       </div>
-      <div className="btn_games" onClick={() => navigate("/game5")}>
+      <div
+        className="btn_games"
+        onClick={() => {
+          goBeforeGame(5);
+        }}
+      >
         <img src={gameImg5} />
         <div className="game_text">알맞은 글자 연결</div>
       </div>

949b3d22f1c4e811ef17d7d75f7ce93847b7d886
Hj1218 <wjdguswl2341@naver.com>
2024-04-10 20:28:36 +0900

    [feat] chart text 띄우기

--- src/component/Graph.js
+++ src/component/Graph.js
@@ -1,30 +1,51 @@
 import React, { useEffect, useRef } from 'react';
-import { Chart } from 'chart.js';
+import { Chart } from 'chart.js/auto';
 
 const Graph = () => {
   const chartRef = useRef(null);
 
   useEffect(() => {
-    const chartInstance = new Chart(chartRef.current, {
+    const data = {
+      datasets: [{
+        label: 'Weekly Sales',
+        data: [17, 15],
+        backgroundColor: [
+          'rgba(255, 26, 104, 0.2)',
+          'rgba(0, 0, 0, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 26, 104, 1)',
+          'rgba(0, 0, 0, 1)'
+        ],
+        borderWidth: 1,
+        cutout: "90%",
+        circumference: 180,
+        rotation: 270
+      }]
+    };
+
+    const gaugeChartText = {
+      id: 'gaugeChartText',
+      afterDatasetsDraw(chart, args, pluginOptions){
+        const{ctx, data, chartArea: {top, bottom, left, right, width, height}, scales: {r} } = chart;
+
+        ctx.save();
+        const xCoor = chart.getDatasetMeta(0).data[0].x;
+        const yCoor = chart.getDatasetMeta(0).data[0].y;
+        ctx.fillStyle = '#666';
+
+        ctx.font = '120px sans-serif';
+        ctx.textAlign='center';
+        ctx.textBaseLine = 'bottom';
+        ctx.fillText('850',xCoor, yCoor);
+      }
+    };
+
+    const config = {
       type: 'doughnut',
-      data: {
-        datasets: [{
-          data: [17, 15],
-          backgroundColor: [
-            'rgba(46, 204, 113, 1)',
-            'rgba(231, 76, 60, 1)'
-          ],
-          borderColor: [
-            'rgba(255, 255, 255 ,1)',
-            'rgba(255, 255, 255 ,1)'
-          ],
-          borderWidth: 5,
-          circumference: 180,
-          rotation: 270,
-          cutout: "80%"
-        }]
-      },
+      data,
       options: {
+        aspectRatio: 1.5,
         plugins: {
           legend: {
             display: false
@@ -32,28 +53,19 @@ const Graph = () => {
           tooltip: {
             enabled: false
           },
-          doughnutlabel: {
-            labels: [
-              {
-                text: '32%',
-                font: {
-                  size: '20'
-                },
-                color: '#000',
-                position: 'center'
-              }
-            ]
-          }
         }
-      }
-    });
+      },
+      plugins: [gaugeChartText]
+    };
+
+    const chartInstance = new Chart(chartRef.current, config);
 
     return () => {
       chartInstance.destroy();
     };
   }, []);
 
-  return <canvas ref={chartRef} style={{ width: '100%' }}/>; 
+  return <canvas ref={chartRef} />;
 };
 
 export default Graph;

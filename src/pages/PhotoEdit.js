7024375b09415d976e79df4590b0524ccc644a11
heedong12 <dlgmldus3289@naver.com>
2024-04-15 19:43:56 +0900

    [feat]버튼 클릭시 배경 변경

--- src/pages/PhotoEdit.js
+++ src/pages/PhotoEdit.js
@@ -43,15 +43,41 @@ const PhotoEdit = ({}) => {
     };
   }, []);
 
+  // const list = ["없음", "해", "밤", "비", "눈", "구름", "바람", "낙엽", "벚꽃"];
+  const list = [
+    { name: "없음", url: "/" },
+    { name: "낮", url: "/day" },
+    { name: "밤", url: "/night" },
+    { name: "비", url: "/rain" },
+    { name: "눈", url: "/snow" },
+    { name: "구름", url: "/cloud" },
+    { name: "바람", url: "/wind" },
+    { name: "낙엽", url: "/leave" },
+    { name: "벚꽃", url: "/blossom" },
+  ];
+  const [backgroundUrl, setBackgroundUrl] = useState("");
+
+  const changeBg = (url) => {
+    console.log(url);
+    setBackgroundUrl(`${url}.png`);
+  };
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <div className={"border-4 border-[#D9D9D9] my-10 p-1"}>
+    <div className={"flex flex-col w-11/12 mx-auto my-5 text-xl"}>
+      <p className={"mb-4"}>배경</p>
+      <div className={"whitespace-nowrap overflow-x-scroll "}>
+        {list.map((item, index) => (
+          <button
+            key={index}
+            className={
+              "bg-[#EDEDED] px-5 py-2 rounded-xl h-12 mr-2 justify-center"
+            }
+            onClick={() => changeBg(item.url)}
+          >
+            {item.name}
+          </button>
+        ))}
+      </div>
+      <div className={"border-4 border-[#D9D9D9] my-10"}>
         <div
           id="limit"
           style={{
@@ -60,6 +86,8 @@ const PhotoEdit = ({}) => {
             display: "flex",
             flexWrap: "wrap",
             alignContent: "flex-start",
+            backgroundImage: `linear-gradient(to bottom, rgba(255,255,255,0.7), rgba(255,255,255,0.7)), url(${process.env.PUBLIC_URL}/Background${backgroundUrl})`,
+            backgroundSize: "cover",
           }}
         >
           {images.length > 0 ? (
@@ -78,7 +106,9 @@ const PhotoEdit = ({}) => {
           )}
         </div>
       </div>
-      <Button width="350px" height="50px" text="완료" onClick={captureImage} />
+      <div className={"flex w-full items-center justify-center"}>
+        <Button width="100%" height="50px" text="완료" onClick={captureImage} />
+      </div>
     </div>
   );
 };

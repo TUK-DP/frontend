6b24bce43fdc4f21b20719647ac91430acb80270
heedong12 <dlgmldus3289@naver.com>
2024-04-15 20:56:02 +0900

    [feat]버튼 선택 시 색상 안바뀌는 오류 해결 및 주석

--- src/pages/PhotoEdit.js
+++ src/pages/PhotoEdit.js
@@ -42,7 +42,7 @@ const PhotoEdit = ({}) => {
       window.removeEventListener("resize", resizeListener);
     };
   }, []);
-
+  // 배경 리스트
   const list = [
     { name: "없음", url: "/" },
     { name: "낮", url: "/day" },
@@ -56,27 +56,46 @@ const PhotoEdit = ({}) => {
   ];
   const [backgroundUrl, setBackgroundUrl] = useState("");
   const [selectedButtonIndex, setSelectedButtonIndex] = useState(0);
-
+  // 배경 변경
   const changeBg = (url, index) => {
     setBackgroundUrl(`${url}.png`);
     setSelectedButtonIndex(index);
   };
+
+  // 배경 투명도
+  const bgOpacity = ["30%", "50%", "70%", "100%"];
   return (
     <div className={"flex flex-col w-11/12 mx-auto my-5 text-xl"}>
+      {/* 배경 선택 */}
       <p className={"mb-4"}>배경</p>
       <div className={"whitespace-nowrap overflow-x-scroll "}>
         {list.map((item, index) => (
           <button
             key={index}
             className={`${
-              selectedButtonIndex === index ? "bg-[#B0B0B0]" : ""
-            }  bg-[#EDEDED] px-5 py-2 rounded-xl h-12 mr-2 justify-center`}
+              selectedButtonIndex === index ? "bg-[#B0B0B0]" : "bg-[#EDEDED]"
+            }   px-5 py-2 rounded-xl h-12 mr-2 justify-center`}
             onClick={() => changeBg(item.url, index)}
           >
             {item.name}
           </button>
         ))}
       </div>
+      {/* 배경 투명도 설정 */}
+      {/* <p className={"mb-4"}>배경 투명도 설정</p>
+      <div className={"whitespace-nowrap overflow-x-scroll "}>
+        {bgOpacity.map((item, index) => (
+          <button
+            key={index}
+            className={
+              " bg-[#EDEDED] px-5 py-2 rounded-xl h-12 mr-2 justify-center"
+            }
+          >
+            {item}
+          </button>
+        ))}
+      </div> */}
+      {/* 그림 편집기 */}
       <div className={"border-4 border-[#D9D9D9] my-10"}>
         <div
           id="limit"
@@ -90,6 +109,7 @@ const PhotoEdit = ({}) => {
             backgroundSize: "cover",
           }}
         >
+          {/* 키워드 별 그림들 배치 */}
           {images.length > 0 ? (
             images.map((image, index) => (
               <Image
@@ -106,6 +126,7 @@ const PhotoEdit = ({}) => {
           )}
         </div>
       </div>
+      {/* 완료 버튼 */}
       <div className={"flex w-full items-center justify-center"}>
         <Button width="100%" height="50px" text="완료" onClick={captureImage} />
       </div>

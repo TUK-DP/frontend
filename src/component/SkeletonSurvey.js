5a7bb6cd4e68e6ac515fb4ab96bb21bdd6bd439e
Hj1218 <wjdguswl2341@naver.com>
2024-04-29 19:34:05 +0900

    [feat] Skeleton UI 디자인

--- src/component/SkeletonSurvey.js
+++ src/component/SkeletonSurvey.js
@@ -1,8 +1,12 @@
 import React from "react";
 const SkeletonSurvey = () => {
   return (
-    <div>
+    <div className="bg-[#ddd] my-10 p-4 rounded-3xl">
+      <div className="bg-[#c6c6c6] h-8 rounded-3xl mb-3"></div>
+      <div className="bg-[#c6c6c6] h-8 rounded-3xl mb-4"></div>
       <div>
+        <div className="h-14 p-4 mb-4 rounded-3xl space-y-3 bg-[#c6c6c6]"></div>
+        <div className="h-14 p-4 rounded-3xl space-y-3 bg-[#c6c6c6]"></div>
       </div>
     </div>
   );

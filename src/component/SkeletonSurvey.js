e081c8ef6ec332c462219bcc06d4637ed2cf6998
Hj1218 <wjdguswl2341@naver.com>
2024-04-29 23:38:49 +0900

    [feat] Skeleton UI 설문 개수에 맞게 생성

--- src/component/SkeletonSurvey.js
+++ src/component/SkeletonSurvey.js
@@ -1,13 +1,22 @@
 import React from "react";
+
 const SkeletonSurvey = () => {
+  const dummyQuestions = Array.from({ length: 32 }, (_, index) => index + 1);
+
   return (
-    <div className="bg-[#ddd] my-10 p-4 rounded-3xl">
-      <div className="bg-[#c6c6c6] h-8 rounded-3xl mb-3"></div>
-      <div className="bg-[#c6c6c6] h-8 rounded-3xl mb-4"></div>
-      <div>
-        <div className="h-14 p-4 mb-4 rounded-3xl space-y-3 bg-[#c6c6c6]"></div>
-        <div className="h-14 p-4 rounded-3xl space-y-3 bg-[#c6c6c6]"></div>
-      </div>
+    <div>
+      {dummyQuestions.map((questionIndex) => (
+        <div key={questionIndex}>
+          <div className="bg-[#ddd] my-10 p-4 rounded-3xl">
+            <div className="bg-[#c6c6c6] h-6 rounded-3xl mb-3"></div>
+            <div className="bg-[#c6c6c6] h-6 rounded-3xl mb-4"></div>
+            <div>
+              <div className="h-14 p-4 mb-4 rounded-3xl space-y-3 bg-[#c6c6c6]"></div>
+              <div className="h-14 p-4 rounded-3xl space-y-3 bg-[#c6c6c6]"></div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };

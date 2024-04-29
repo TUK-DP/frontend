f8d6032bd3f3fb8e57347fd580ee2f920f63d6a6
Hj1218 <wjdguswl2341@naver.com>
2024-04-30 00:35:20 +0900

    [feat] Shimmer 삭제

--- src/component/SkeletonSurvey.js
+++ src/component/SkeletonSurvey.js
@@ -1,20 +1,18 @@
 import React from "react";
 import "../styles/Skeleton.css";
-import Shimmer from "./Shimmer";
 
 const SkeletonSurvey = () => {
   const dummyQuestions = Array.from({ length: 32 }, (_, index) => index + 1);
-
   return (
     <div>
       {dummyQuestions.map((questionIndex) => (
         <div key={questionIndex}>
           <div className="bg-[#ddd] my-10 pt-4 px-4 pb-1 rounded-3xl">
-            <div className="skeletonText"><Shimmer /></div>
-            <div className="skeletonText"><Shimmer /></div>
+            <div className="skeletonText"></div>
+            <div className="skeletonText"></div>
             <div>
-              <div className="skeletonBox"><Shimmer /></div>
-              <div className="skeletonBox"><Shimmer /></div>
+              <div className="skeletonBox"></div>
+              <div className="skeletonBox"></div>
             </div>
           </div>
         </div>

4e89bd9a5991cfdbaa5536f52cf7548698063718
Hj1218 <wjdguswl2341@naver.com>
2024-04-30 00:01:38 +0900

    [feat] Skeleton UI CSS 수정

--- src/component/SkeletonSurvey.js
+++ src/component/SkeletonSurvey.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/Skeleton.css";
 
 const SkeletonSurvey = () => {
   const dummyQuestions = Array.from({ length: 32 }, (_, index) => index + 1);
@@ -7,12 +8,12 @@ const SkeletonSurvey = () => {
     <div>
       {dummyQuestions.map((questionIndex) => (
         <div key={questionIndex}>
-          <div className="bg-[#ddd] my-10 p-4 rounded-3xl">
-            <div className="bg-[#c6c6c6] h-6 rounded-3xl mb-3"></div>
-            <div className="bg-[#c6c6c6] h-6 rounded-3xl mb-4"></div>
+          <div className="bg-[#ddd] my-10 pt-4 px-4 pb-1 rounded-3xl">
+            <div className="skeletonText"></div>
+            <div className="skeletonText"></div>
             <div>
-              <div className="h-14 p-4 mb-4 rounded-3xl space-y-3 bg-[#c6c6c6]"></div>
-              <div className="h-14 p-4 rounded-3xl space-y-3 bg-[#c6c6c6]"></div>
+              <div className="skeletonBox"></div>
+              <div className="skeletonBox"></div>
             </div>
           </div>
         </div>

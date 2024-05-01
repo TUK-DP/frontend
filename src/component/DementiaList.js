6d62f6cec181e51017e65321b1ec54b2a3b5cb4b
Hj1218 <wjdguswl2341@naver.com>
2024-05-01 22:03:44 +0900

    [feat] centerMap 페이지 연결, 정보 넘겨받기

--- src/component/DementiaList.js
+++ src/component/DementiaList.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const DementiaList = ({ centers }) => {
+  const navigate = useNavigate();
+
+  const handlesubmit = (index) => {
+    navigate("/centermap", { state: { centers, index } });
+  };
+
   return (
     <div>
       {centers.map((center, index) => (
-        <div key={index} className="flex flex-col px-2.5 py-2 gap-2.5 border-b-2 border-neutral-300 text-lg mb-3">
+        <div key={index} className="flex flex-col px-2.5 py-2 gap-2.5 border-b-2 border-neutral-300 text-lg mb-3" onClick={() => handlesubmit(index)}>
           <div className="flex justify-between">
-          <div className="text-[#82AAE3]">{center.name}</div>
-          <div className={"font-semibold"}>{center.distance.toFixed(1)}km</div>
+            <div className="text-[#82AAE3]">{center.name}</div>
+            <div className={"font-semibold"}>{center.distance.toFixed(1)}km</div>
           </div>
           <div className={"flex flex-row gap-5"}>
             <div>{center.address}</div>
--- src/pages/CenterMap.js
+++ src/pages/CenterMap.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const CenterMap = () => {
+  const location = useLocation();
+  const { centers, index } = location.state;
+  const center = centers[index];
+
   return (
     <div>
+      <div>
+        <p>치매 센터 이름: {center.name}</p>
+        <p>거리: {center.distance.toFixed(1)}km</p>
+        <p>주소: {center.address}</p>
+      </div>
     </div>
   );
 };

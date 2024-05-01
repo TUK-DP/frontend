b087c22d59e4314e463e42ad7844c71788f46425
Hj1218 <wjdguswl2341@naver.com>
2024-05-01 22:33:54 +0900

    [feat] Map -> KakaoMap 변경

---
+++ src/component/KakaoMap.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+
+const kakaoMap = ({ latitude, longitude }) => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default kakaoMap;

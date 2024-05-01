7320fe8a89fbc296af3769bd279098a38154e373
Hj1218 <wjdguswl2341@naver.com>
2024-05-01 21:26:30 +0900

    [feat] Dementia List 퍼블리싱

--- src/component/DementiaList.js
+++ src/component/DementiaList.js
@@ -4,10 +4,12 @@ const DementiaList = ({ centers }) => {
   return (
     <div>
       {centers.map((center, index) => (
-        <div key={index}>
-          {center.name}
-          <div>
-            <div>{center.distance.toFixed(1)}km</div>
+        <div key={index} className="flex flex-col px-2.5 py-2 gap-2.5 border-b-2 border-neutral-300 text-lg mb-3">
+          <div className="flex justify-between">
+          <div className="text-[#82AAE3]">{center.name}</div>
+          <div className={"font-semibold"}>{center.distance.toFixed(1)}km</div>
+          </div>
+          <div className={"flex flex-row gap-5"}>
             <div>{center.address}</div>
           </div>
         </div>

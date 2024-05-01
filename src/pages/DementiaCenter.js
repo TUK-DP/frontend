bcf955f61eaa4e70b69aecccc4c5fb609d48bb0f
Hj1218 <wjdguswl2341@naver.com>
2024-05-01 18:14:59 +0900

    [feat] 현재 위치 위도, 경도 출력

--- src/pages/DementiaCenter.js
+++ src/pages/DementiaCenter.js
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Search from "../assets/search.png";
 import DementiaList from "../component/DementiaList";
 
 const DementiaCenter = () => {
+  const [latitude, setLatitude] = useState(null); //위도
+  const [longitude, setLongitude] = useState(null); //경도
+
+  useEffect(() => {
+    const getCurrentLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            setLatitude(position.coords.latitude);
+            setLongitude(position.coords.longitude);
+          },
+          (error) => {
+            console.error("Error getting geolocation:", error);
+          }
+        );
+      } else {
+        console.error("Geolocation is not supported by this browser.");
+      }
+    };
+
+    getCurrentLocation();
+    if (latitude !== null && longitude !== null) {
+      console.log("Latitude:", latitude); //위도
+      console.log("Longitude:", longitude); //경도
+    }
+  }, []);
+
   return (
     <div className={"flex flex-col justify-start px-2.5 py-5 h-full gap-5"}>
       {/* 검색바 */}
       <div className="flex flex-row items-center border border-1 rounded-2xl gap-5 px-2.5 h-12 border-black">
-        <img src={Search} className={"h-9 w-9"} />
         <input
           className="flex-grow border-none"
-          placeholder="위치를 입력해주세요."
+          placeholder="거리(km)를 입력해주세요."
         />
+        <img src={Search} className={"h-9 w-9"} />
       </div>
       {/* 치매센터리스트 */}
       <div className={"flex flex-col px-2.5"}>

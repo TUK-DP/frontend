f55f445fa3e2fcf07e5023f009bfa1c43bd1e3f6
Hj1218 <wjdguswl2341@naver.com>
2024-05-01 22:36:08 +0900

    [feat] 컴포넌트 대문자로 수정

--- src/pages/CenterMap.js
+++ src/pages/CenterMap.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import kakaoMap from "../component/KakaoMap";
+import KakaoMap from "../component/KakaoMap";
 
 const CenterMap = () => {
   const location = useLocation();
@@ -11,7 +11,7 @@ const CenterMap = () => {
     <div>
       <div className="text-center font-bold text-[#82AAE3] text-2xl my-5">{center.name}</div>
       <div className="w-full h-auto">
-      <kakaoMap latitude={center.latitude} longitude={center.longitude} />
+      <KakaoMap latitude={center.latitude} longitude={center.longitude} />
       </div>
       <div className="text-lg">거리: {center.distance.toFixed(1)}km</div>
       <div className="text-lg">주소: {center.address}</div>

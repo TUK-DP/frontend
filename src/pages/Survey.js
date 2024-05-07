fa6a57d2c53cb679aa5e636687b04399ca6ae316
Hj1218 <wjdguswl2341@naver.com>
2024-05-07 23:10:39 +0900

    [refactor] 한페이지당 질문 하나, 이전 페이지로 돌아가도 선택 반영

--- src/pages/Survey.js
+++ src/pages/Survey.js
@@ -1,86 +1,100 @@
 import React, { useEffect, useState } from "react";
 import SurveyController from "../api/survey.controller";
-import Button from "../component/Button";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { SET_PAGENAME } from "../redux/modules/PageName";
 import SkeletonSurvey from "../component/SkeletonSurvey";
+import { FaRegCircle } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
+import { FaChevronRight } from "react-icons/fa6";
+import { FaChevronLeft } from "react-icons/fa6";
+import Button from "../component/Button";
 
-function SurveyCop({ question, onVote }) {
+function SurveyCop({ question, selectedResults, setSelectedResults }) {
   const [selectedOption, setSelectedOption] = useState(null);
 
-  const handleVote = (option) => {
-    setSelectedOption(option);
-    onVote(question.index, option);
+  useEffect(() => {
+    if (selectedResults[question.index] !== null) {
+      setSelectedOption(selectedResults[question.index]);
+    }
+  }, [selectedResults, question.index]);
+
+  const handleVote = (questionIndex, option) => {
+    const updatedResults = [...selectedResults];
+    updatedResults[questionIndex] = option;
+    setSelectedResults(updatedResults);
   };
 
   return (
-    <div
-      className={
-        "bg-[#e0f4ff] my-10 p-4 text-2xl rounded-3xl space-y-3 shadow-lg"
-      }
-    >
-      <div className={"flex"}>
+    <div>
+      <div className={"flex bg-[#e0f4ff] my-10 p-6 text-3xl rounded-3xl space-y-3 shadow-lg"}>
         <p>
           {question.index}. {question.question}
         </p>
       </div>
-      <div
-        className={
-          "pl-4 w-full h-14 bg-white rounded-2xl mb-4 flex items-center"
-        }
-        style={{
-          backgroundColor: selectedOption === "O" ? "#C6F6D5" : "white",
-        }}
-        onClick={() => handleVote("O")}
-      >
-        1. O
-      </div>
-      <div
-        className={"pl-4 w-full h-14 bg-white rounded-2xl flex items-center"}
-        style={{
-          backgroundColor: selectedOption === "X" ? "#C6F6D5" : "white",
-        }}
-        onClick={() => handleVote("X")}
-      >
-        2. X
+      <div className="flex w-full justify-between">
+        <div
+          className={
+            "w-[45%] h-[12rem] bg-white rounded-2xl flex items-center text-2xl rounded-3xl border-4 justify-center mb-5"
+          }
+          style={{
+            backgroundColor: selectedOption === "O" ? "#C6F6D5" : "white",
+          }}
+          onClick={() => handleVote(question.index, "O")}
+        >
+          <FaRegCircle size={"80px"} />
+        </div>
+        <div
+          className={"w-[45%] h-[12rem] bg-white rounded-2xl flex items-center text-2xl rounded-3xl border-4 justify-center mb-5"}
+          style={{
+            backgroundColor: selectedOption === "X" ? "#C6F6D5" : "white",
+          }}
+          onClick={() => handleVote(question.index, "X")}
+        >
+          <RxCross2 size={"100px"} />
+        </div>
       </div>
     </div>
   );
 }
 
-function SurveyList({ questionState, onVote }) {
+function SurveyList({ questionState, currentQuestionIndex, onVote, selectedResults, setSelectedResults }) {
   return (
-    <>
-      {questionState.map((q, index) => {
-        return <SurveyCop key={index} question={q} onVote={onVote} />;
-      })}
-    </>
+    <SurveyCop
+      key={currentQuestionIndex}
+      question={questionState[currentQuestionIndex]}
+      onVote={onVote}
+      selectedResults={selectedResults}
+      setSelectedResults={setSelectedResults}
+    />
   );
 }
+
 
 function Survey() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch({ type: SET_PAGENAME, pageName: "치매진단" });
   }, []);
+
   const navigate = useNavigate();
   const [questionState, setQuestionState] = useState([]);
-  const [selectedResults, setSelectedResults] = useState([]);
+  const [selectedResults, setSelectedResults] = useState(Array(32).fill(null));
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [checkList, setCheckList] = useState(true);
-  const [isLoading, setIsLoading] = useState(true); 
+  const [isLoading, setIsLoading] = useState(true);
 
   const get_survey = async () => {
     const survey = await SurveyController.findAllSimpleSurvey();
     setQuestionState(survey.data.data);
     setSelectedResults(new Array(survey.data.data.length).fill(null));
-    setIsLoading(false); 
+    setIsLoading(false);
   };
 
   useEffect(() => {
     setTimeout(() => {
       get_survey();
-    }, 1000); 
+    }, 1000);
   }, []);
 
   const handleVote = (questionIndex, option) => {
@@ -91,6 +105,18 @@ function Survey() {
     setSelectedResults(updatedResults);
   };
 
+  const handleNextQuestion = () => {
+    if (currentQuestionIndex < questionState.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    }
+  };
+
+  const handlePreviousQuestion = () => {
+    if (currentQuestionIndex > 0) {
+      setCurrentQuestionIndex(currentQuestionIndex - 1);
+    }
+  };
+
   const handleSubmit = () => {
     const oCount = selectedResults.filter((result) => result === "O").length;
     const xCount = selectedResults.filter((result) => result === "X").length;
@@ -98,8 +124,6 @@ function Survey() {
     console.log(`Total X Count: ${xCount}`);
     if (oCount + xCount === 32) {
       navigate("/surveyresult", { state: { oCount, xCount } });
-    } else {
-      setCheckList(false);
     }
   };
 
@@ -109,16 +133,28 @@ function Survey() {
         <SkeletonSurvey />
       ) : (
         <>
-          <SurveyList questionState={questionState} onVote={handleVote} />
-          <Button
-            height={"60px"}
-            text={"제출하기"}
-            fontSize={"24px"}
-            onClick={handleSubmit}
+          <SurveyList
+            questionState={questionState}
+            currentQuestionIndex={currentQuestionIndex}
+            onVote={handleVote}
+            selectedResults={selectedResults}
+            setSelectedResults={setSelectedResults}
           />
+          <div className={"flex w-full justify-between px-8 mb-[2rem]"}>
+            {currentQuestionIndex === 0 ? (
+              <div></div>
+            ) : (
+              <FaChevronLeft  onClick={handlePreviousQuestion} size={"50px"}/>
+            )}
+            {currentQuestionIndex === 31 ? (
+              <div></div>
+            ) : (
+              <FaChevronRight onClick={handleNextQuestion} size={"50px"}/>
+            )}
+          </div>
           {!checkList ? (
             <div className="text-[#e15449] mt-2 text-xl">
-              모든 문항을 체크해주세요.
+              문항을 체크해주세요.
             </div>
           ) : null}
         </>

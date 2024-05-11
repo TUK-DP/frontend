e29ad9789dfc021e09d7a50349fd2db85cf8d3c5
heedong12 <dlgmldus3289@naver.com>
2024-05-11 15:34:06 +0900

    [feat]사진 편집 moveable로 변경

--- src/component/ImageDiary/Image.js
+++ src/component/ImageDiary/Image.js
@@ -1,44 +1,67 @@
 import React, { useState, useRef, useEffect } from "react";
 import Draggable from "react-draggable";
+import Moveable from "react-moveable";
+import MoveableHelper from "moveable-helper";
 
 const Image = ({ image, initialPo, width }) => {
-  const nodeRef = useRef(null);
-  const [position, setPosition] = useState({ x: initialPo.x, y: initialPo.y });
+  const targetRef = useRef(null);
+  const [helper] = useState(() => {
+    return new MoveableHelper();
+  });
+  // const [position, setPosition] = useState({ x: initialPo.x, y: initialPo.y });
 
-  const trackPos = (data) => {
-    setPosition({
-      x: initialPo.x + data.x,
-      y: initialPo.y + data.y,
-    });
-  };
+  // const trackPos = (data) => {
+  //   setPosition({
+  //     x: initialPo.x + data.x,
+  //     y: initialPo.y + data.y,
+  //   });
+  // };
 
-  useEffect(() => {
-    if (
-      position.x < 0 ||
-      position.y < 0 ||
-      position.x > width ||
-      position.y > width
-    ) {
-      alert("캔버스 안에서만 움직여주세요.");
-    }
-    console.log("x: ", position.x, "y: ", position.y);
-  }, [position.x, position.y]);
+  // useEffect(() => {
+  //   if (
+  //     position.x < 0 ||
+  //     position.y < 0 ||
+  //     position.x > width ||
+  //     position.y > width
+  //   ) {
+  //     alert("캔버스 안에서만 움직여주세요.");
+  //   }
+  //   console.log("x: ", position.x, "y: ", position.y);
+  // }, [position.x, position.y]);
 
   return (
-    <Draggable
-      bounds={{
-        top: -initialPo.y,
-        left: -initialPo.x,
-        right: width - initialPo.x - 100,
-        bottom: width - initialPo.y - 100,
-      }}
-      nodeRef={nodeRef}
-      onDrag={(e, data) => trackPos(data)}
-    >
-      <div ref={nodeRef} style={{ width: "100px", height: "100px" }}>
-        <img draggable={"false"} src={image} width="100px" height="100px" />
+    // <Draggable
+    //   bounds={{
+    //     top: -initialPo.y,
+    //     left: -initialPo.x,
+    //     right: width - initialPo.x - 100,
+    //     bottom: width - initialPo.y - 100,
+    //   }}
+    //   nodeRef={nodeRef}
+    //   onDrag={(e, data) => trackPos(data)}
+    // >
+    //   <div ref={nodeRef} style={{ width: "100px", height: "100px" }}>
+    //     <img draggable={"false"} src={image} width="100px" height="100px" />
+    //   </div>
+    // </Draggable>
+    <div>
+      <div className="target" ref={targetRef}>
+        <img src={image} width={100} height={100} />
       </div>
-    </Draggable>
+      <Moveable
+        target={targetRef}
+        draggable={true}
+        scalable={true}
+        keepRatio={true}
+        rotatable={true}
+        onDragStart={helper.onDragStart}
+        onDrag={helper.onDrag}
+        onScaleStart={helper.onScaleStart}
+        onScale={helper.onScale}
+        onRotateStart={helper.onRotateStart}
+        onRotate={helper.onRotate}
+      />
+    </div>
   );
 };
 

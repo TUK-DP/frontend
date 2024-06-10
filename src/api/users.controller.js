d245027d991c3a0e9973015d1ac8de9f138ac529
Hj1218 <wjdguswl2341@naver.com>
2024-06-10 21:16:38 +0900

    [feat] 회원정보 수정 patch -> put 변경

--- src/api/users.controller.js
+++ src/api/users.controller.js
@@ -26,8 +26,8 @@ class UserController extends Api {
     });
   };
   // 회원 정보 수정
-  updateUser = async ({ userId, userData }) => {
-    return await this.patch(`/users/${userId}`, { data: userData });
+  updateUser = async (userData) => {
+    return await this.put("/users", { data: userData });
   };
   // 회원탈퇴
 }

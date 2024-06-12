56bd6f439674d2dcc2e5a0ec3b037117067a737c
Hj1218 <wjdguswl2341@naver.com>
2024-06-12 15:02:41 +0900

    [feat] 일기 조회 기능 추가

--- src/pages/DiaryContent.js
+++ src/pages/DiaryContent.js
@@ -1,7 +1,16 @@
 import React from "react";
 import "../styles/diary.css";
+import diaryController from "../api/diary.controller";
 
 const DiaryContent = () => {
+  const getDiary = () => {
+    try {
+      const response = diaryController.searchDiary(3, "2024-05-13");
+      console.log(response.data);
+    } catch (error) {
+      console.error("일기 조회 중 오류", error);
+    }
+  };
   return (
     <div id="diary">
       <div className="text-xl font-bold mt-4">일기 날짜</div>
